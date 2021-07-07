342b9302cb8b6798f0a1d6a35ae892685a8f20cf
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2021-07-07 15:18:36 -0400

    dockerized

--- api/app.js
+++ api/app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const path = require('path');
 const errorHandler = require('./errors/errorHandler');
 const cors = require('cors')
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const rushingYardDataRouter = require('./routes/rushingYardData');
@@ -16,10 +14,8 @@ app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('helpers', path.join(__dirname, 'helpers'));
 app.set('view engine', 'jade');
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -30,7 +26,6 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use(errorHandler);
 
 module.exports = app;
